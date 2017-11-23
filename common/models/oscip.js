67082f65b304b86d1afe41f50dc2557ddfb37ab4
Carlos Damázio <kaduardobsb95@gmail.com>
2017-11-23 20:34:52 -0200

    Loopback initialization.
    New project set for loopback API server with OSCIP model set.
    Request filter for GET is not working properly, so it must
    be fixed.

---
+++ common/models/oscip.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(Oscip) {
+  Oscip.validatesPresenceOf(
+    'cnpj',
+    'entity_name',
+    'address',
+    'cep',
+    'city',
+    'phone',
+    'uf',
+    'publications',
+    'purposes'
+  );
+  Oscip.validate('cnpj', 'cep', 'phone', digitValidator, {
+    message: 'Não são números!',
+  });
+  function digitValidator(err) {
+    if (isNaN(this.cnpj) || isNaN(this.cep) || isNaN(this.phone)) err();
+  };
+  Oscip.validatesUniquenessOf('cnpj', {message: 'CNPJ já está cadastrado!'});
+  Oscip.validatesLengthOf('cnpj', {is: 14, message: {
+    is: 'CNPJ tem que possuir exatamente 14 dígitos!'},
+  });
+  Oscip.validatesLengthOf('cep', {is: 8, message: {
+    is: 'CEP tem que possuir exatamente 8 dígitos!'},
+  });
+  Oscip.validatesLengthOf('uf', {is: 2, message: {
+    is: 'Unidade federativa tem que ter 2 letras apenas!'},
+  });
+};
